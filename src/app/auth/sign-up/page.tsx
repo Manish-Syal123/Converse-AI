d9c5fe7e8677ef756ffac909238d923220ff8674
Manish_Syal <manishsyal77@gmail.com>
2024-09-16 16:53:47 +0530

    Designed the first step of user signup -->uI

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+import SignUpFormProvider from "@/components/forms/sign-up/form-provider";
+import RegistrationFormStep from "@/components/forms/sign-up/registration-step";
+
 import React from "react";
 
 type Props = {};
 
 const SignUp = (props: Props) => {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex-1 py-36 md:px-16 w-full">
+      <div className="flex flex-col h-full gap-3">
+        <SignUpFormProvider>
+          <div className="flex flex-col gap-3">
+            <RegistrationFormStep></RegistrationFormStep>
+            {/* <ButtonHandler /> */}
+          </div>
+          {/* <HighLightBar /> */}
+        </SignUpFormProvider>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
